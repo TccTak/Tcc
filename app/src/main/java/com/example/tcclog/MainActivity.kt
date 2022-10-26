649a5f61183bed1bb519af62f7ecdf715993ef36
TccTak <116512649+TccTak@users.noreply.github.com>
2022-10-26 15:43:15 -0300

    New modifications

--- app/src/main/java/com/example/tcclog/MainActivity.kt
+++ app/src/main/java/com/example/tcclog/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,3 +12,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
